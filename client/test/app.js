b2f9ede22a08474aa2b7685d33ca925baa368145
Stanislas Bernard <stanislasb@theodo.fr>
2016-06-08 16:23:43 +0200

    Fix tests

--- client/test/app.js
+++ client/test/app.js
@@ -1,12 +1,33 @@
 import {expect} from 'chai';
-import {addItem, removeItem, modifyItem} from '../src/app';
 
-// Angular mock to avoid big test frameworks
-const angular = {
-  controller: () => {}
+// Js dom
+import jsdom from 'jsdom';
+global.document = jsdom.jsdom('<!doctype html><html><body></body></html>');
+global.window = document.defaultView;
+global.navigator = {
+    userAgent: 'node.js'
+};
+const angular = require('../bower_components/angular/angular');
+
+// take all properties of the window object and also attach it to the
+// from mocha-jsdom https://github.com/rstacruz/mocha-jsdom/blob/master/index.js#L80
+function propagateToGlobal (window) {
+    for (const key in window) {
+        if (!window.hasOwnProperty(key)) {continue; }
+        if (key in global) {continue; }
+        global[key] = window[key];
+    }
 }
-angular.module = () => angular;
-global.angular = angular;
+// mocha global object
+propagateToGlobal(window);
+
+process.on('unhandledRejection', (error)=>{
+    console.error('Unhandled Promise Rejection:');
+    console.error(error && error.stack || error);
+});
+
+const app = require('../src/app');
+const {addItem, removeItem, modifyItem} = app;
 
 const items = [
   {
